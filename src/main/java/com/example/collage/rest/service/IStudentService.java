e365d429f3312e4830c6a5ea86dee69a1d12b700
Sharmi1712 <77524058+Sharmi1712@users.noreply.github.com>
2021-01-20 21:57:26 +0530

    Update IStudentService.java

--- src/main/java/com/example/collage/rest/service/IStudentService.java
+++ src/main/java/com/example/collage/rest/service/IStudentService.java
@@ -1,10 +1,14 @@
 package com.example.collage.rest.service;
 
+import java.util.List;
+
 import com.example.collage.rest.model.Student;
 
 public interface IStudentService {
 
 	public Student get(long id);
+	
+	public List<Student> getAll();
 	
 	public String delete(long id);
 	
